e78d3560a53f553128b68084d069ab6db0369152
Adrian Gheorghe <adrian.gheorghe@gmail.com>
2012-08-10 10:44:48 +0200

    Initial commit

---
+++ samples/phpbb.php
@@ -0,0 +1,549 @@
+<?php
+
+// Change this to a random string (between 32-64 characters)
+define('SMCC_SDK_ACCESS_TOKEN', 'MtbekP4DseWrXX0xtaOcrEhrU2tjTV6F84O8nw0GqC79SIFESjjZ6rqsOec24dw');
+
+// Don't change anything else below
+///////////////////////////////////////////////////////////////////////
+
+// Needed so the other includes work
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+include($phpbb_root_path . 'includes/functions_posting.' . $phpEx);
+include($phpbb_root_path . 'includes/functions_admin.' . $phpEx);
+include($phpbb_root_path . 'includes/functions_privmsgs.' . $phpEx);
+
+$auth = new smcc_sdk_auth;
+$user->data = array(
+    'is_registered' => true,
+    'user_lastmark' => 0
+);
+$user->ip = '';
+
+$sdk = new smcc_sdk;
+$sdk->respond();
+
+// Request handling {{{
+class smcc_sdk
+{
+    protected $api = null;
+
+    public function __construct()
+    {
+        $this->api = new smcc_sdk_api;
+    }
+
+    public function respond()
+    {
+        if (!$this->is_sdk_request())
+        {
+            exit;
+        }
+        $params = $this->get_params();
+        $method = str_replace('.', '_', $params['action']);
+        $this->api->$method($params, $this->get_body());
+    }
+
+    public function is_sdk_request()
+    {
+        $params = $this->get_params();
+
+        $action = $params['action'];
+        $token  = $params['access_token'];
+
+        return !empty($token) && $token === SMCC_SDK_ACCESS_TOKEN && !empty($action) && in_array($action, $this->api->implemented_actions);
+    }
+
+    public function get_params()
+    {
+        $params = array();
+        foreach ($_GET as $k => $v)
+        {
+            if (strpos($k, 'smcc_sdk_') === 0)
+            {
+                $key = str_replace('smcc_sdk_', '', $k);
+                $params[$key] = $v;
+            }
+        }
+        return $params;
+    }
+
+    public function get_body()
+    {
+        $body = file_get_contents('php://input');
+        return empty($body) ? '' : json_decode($body, true);
+    }
+}
+// }}}
+
+// API actions implementations {{{
+class smcc_sdk_api
+{
+    public $implemented_actions = array(
+        'implementation.info',
+        'messages.create', 'messages.destroy', 'messages.list', 'messages.show',
+        'private_messages.create', 'private_messages.list', 'private_messages.show',
+        'threads.destroy', 'threads.show',
+        'users.show',
+        'messages_warn' // custom
+    );
+
+    public $implemented_options = array('threads.no_content');
+
+    protected $db = null;
+
+    public function __construct()
+    {
+        $this->db = new smcc_sdk_db;
+    }
+
+    // Implementation {{{
+    
+    public function implementation_info()
+    {
+        $this->response_success(array(
+            'actions' => $this->implemented_actions,
+            'options' => $this->implemented_options
+        ));
+    }
+
+    // }}}
+
+    // Messages {{{
+
+    // functions_posting.php #1974
+    public function messages_create($params, $body)
+    {
+        global $user;
+
+        $user->data['user_id'] = $body['author_id'];
+        $poll = array();
+        $topic = $this->db->get_topic($body['thread_id']);
+        $subject = $body['title'];
+        $data = array(
+            'topic_title' => '',
+            'poster_id' => $body['author_id'],
+            'topic_id' => $body['thread_id'],
+            'forum_id' => $topic['forum_id'],
+            'icon_id' => 0,
+            'enable_bbcode' => 1,
+            'enable_smilies' => 1,
+            'enable_urls' => 1,
+            'enable_sig' => 1,
+            'message' => $body['body'],
+            'message_md5' => md5($body['body']),
+            'attachment_data' => null,
+            'bbcode_bitfield' => '',
+            'bbcode_uid' => '',
+            'post_edit_locked' => true,
+            'force_approved_state' => true
+        );
+
+        submit_post(
+            'reply', // mode
+            $subject, // subject
+            '', // username
+            POST_NORMAL,
+            $poll, // poll
+            $data
+        );
+
+        $this->messages_show(array('id' => $data['post_id']), '');
+    }
+
+    public function messages_destroy($params, $body)
+    {
+        delete_posts('post_id', $params['id']);
+        $this->response_success(true);
+    }
+
+    public function messages_list($params, $body)
+    {
+        $rows = array();
+        foreach ($this->db->get_messages($params['since_id']) as $message)
+        {
+            $rows[] = $this->format_post($message);
+        }
+        $this->response_success($rows);
+    }
+
+    public function messages_show($params, $body)
+    {
+        $post = $this->db->find_post($params['id']);
+        $this->response_success($this->format_post($post));
+    }
+
+    public function messages_warn($params, $body)
+    {
+        $this->db->add_message_warning($params['id'], $params['user_id']);
+        $this->response_success($this->format_post($this->db->find_post($params['id'])));
+    }
+
+    // }}}
+
+    // Private messages {{{
+
+    // table privmsgs
+    // functions_privmsgs #1300
+    public function private_messages_create($params, $body)
+    {
+        global $user;
+
+        $user->data['user_id'] = $body['author_id'];
+
+        if (empty($body['in_reply_to_id']))
+        {
+            $initial_msg = array();
+        }
+        else
+        {
+            $initial_msg = $this->db->find_privmsg($body['in_reply_to_id']);
+        }
+
+        $subject = $body['title'];
+        $data = array(
+            'address_list' => array('u' => array($body['recipient_id'] => 'to')),
+            'reply_from_root_level' => ($initial_msg && $initial_msg['root_level'] === 0),
+            'reply_from_msg_id' => $body['in_reply_to_id'],
+            'from_user_id' => $body['author_id'],
+            'from_user_ip' => '',
+            'enable_bbcode' => 1,
+            'enable_smilies' => 1,
+            'enable_urls' => 1,
+            'enable_sig' => 1,
+            'message' => $body['body'],
+            'icon_id' => 0,
+            'attachment_data' => null,
+            'bbcode_bitfield' => '',
+            'bbcode_uid' => ''
+        );
+        $put_in_outbox = true;
+
+        submit_pm(
+            $data['reply_from_msg_id'] ? 'reply' : 'post',
+            $subject,
+            $data, 
+            $put_in_outbox
+        );
+        $this->private_messages_show(array('id' => $data['msg_id']), array());
+    }
+
+    public function private_messages_list($params, $body)
+    {
+        $rows = array();
+        if (!empty($params['user_ids']) && is_array($params['user_ids']))
+        {
+            $ids = array();
+            foreach ($params['user_ids'] as $id)
+            {
+                $ids[] = (int)$id;
+            }
+
+            foreach ($this->db->get_private_messages((int)$params['since_id'], $ids) as $message)
+            {
+                $rows[] = $this->format_privmsg($message);
+            }
+        }
+
+        $this->response_success($rows);
+    }
+
+    public function private_messages_show($params, $body)
+    {
+        $msg = $this->db->find_privmsg($params['id']);
+        $this->response_success($this->format_privmsg($msg));
+    }
+
+    // }}}
+
+    // Threads {{{
+
+    // functions_posting #1390
+    public function threads_destroy($params, $body)
+    {
+        delete_topics(
+            'range', // where_type
+            array($params['id']) // where_ids
+        );
+        $this->response_success(true);
+    }
+
+    public function threads_show($params, $body)
+    {
+        $topic = $this->db->get_topic($params['id']);
+        $thread = topic_to_sdk($topic);
+        $thread['author'] = user_to_sdk($topic);
+
+        $this->response_success($thread);
+    }
+
+    // }}}
+
+    // Helpers {{{
+
+    protected function response_success($payload)
+    {
+        exit(json_encode(array('success' => true, 'payload' => $payload)));
+    }
+
+    protected function response_error($message)
+    {
+        exit(json_encode(array('success' => false, 'errorMessage' => $message)));
+    }
+
+    protected function format_post($post)
+    {
+        $record = post_to_sdk($post);
+        $record['author'] = user_to_sdk($post);
+
+        return $record;
+    }
+
+    protected function format_privmsg($msg)
+    {
+        $to_parts = explode(':', $msg['to_address']);
+        $recipient_id = str_replace('u_', '', $to_parts[0]); // I only use the first user
+
+        $record = privmsg_to_sdk($msg);
+        $record['author'] = user_to_sdk($msg);
+        $record['recipient'] = user_to_sdk($this->db->find_user($recipient_id));
+
+        return $record;
+    }
+
+    // }}}
+}
+// }}}
+
+// DB helpers {{{
+class smcc_sdk_db
+{
+    public function __construct()
+    {
+        $sql = 'SELECT p.*, u.*, t.*, f.* FROM ' . POSTS_TABLE . ' p ';
+        $sql .= 'LEFT JOIN ' . USERS_TABLE . ' u ON p.poster_id=u.user_id ';
+        $sql .= 'LEFT JOIN ' . TOPICS_TABLE . ' t ON p.topic_id = t.topic_id ';
+        $sql .= 'LEFT JOIN ' . FORUMS_TABLE . ' f on t.forum_id = f.forum_id ';
+        $this->post_sql = $sql;
+
+        $sql = 'SELECT pm.*, u.* FROM ' . PRIVMSGS_TABLE . ' pm ';
+        $sql .= 'LEFT JOIN ' . USERS_TABLE . ' u ON pm.author_id=u.user_id ';
+        $this->privmsg_sql = $sql;
+    }
+
+    public function add_message_warning($post_id, $user_id)
+    {
+        $post = $this->find_post($post_id);
+
+        $sql = 'SELECT warning_id FROM ' . WARNINGS_TABLE . ' w ';
+        $sql .= 'WHERE w.user_id=' . $post['poster_id'] . ' AND w.post_id=' . $post_id;
+        $warn = $this->find_row($sql);
+        if (empty($warn))
+        {
+            $sql = 'INSERT INTO ' . WARNINGS_TABLE . ' (user_id, post_id, log_id) VALUES ';
+            $sql .= '(' . $post['poster_id'] . ', ' . $post_id . ', 0)';
+            $this->execute($sql);
+        }
+    }
+
+    public function get_messages($since_id)
+    {
+        $sql = $this->post_sql;
+        $sql .= 'WHERE p.post_id > ' . (int)$since_id . ' ';
+        $sql .= 'ORDER BY p.post_id DESC';
+
+        return $this->get_rows($sql);
+    }
+
+    // table phpbb_privmsgs
+    public function get_private_messages($since_id, $user_ids)
+    {
+        $sql = 'SELECT pm.* FROM ' . PRIVMSGS_TO_TABLE . ' pm WHERE pm.msg_id > ' . $since_id . ' AND pm.user_id in (' . implode(',', $user_ids) . ')';
+        $rows = $this->get_rows($sql);
+        if (empty($rows))
+        {
+            return array();
+        }
+
+        $ids = array();
+        foreach ($rows as $row)
+        {
+            $ids[] = $row['msg_id'];
+        }
+
+        $sql = $this->privmsg_sql;
+        $sql .= 'WHERE pm.msg_id in (' . implode(',', array_unique($ids)) . ') ';
+        $sql .= 'ORDER BY pm.msg_id DESC';
+
+        return $this->get_rows($sql);
+    }
+
+    public function find_post($id)
+    {
+        $sql = $this->post_sql;
+		$sql .= 'WHERE p.post_id=' . $id;
+        return $this->find_row($sql);
+    }
+
+    public function find_privmsg($id)
+    {
+        $sql = $this->privmsg_sql;
+        $sql .= 'WHERE pm.msg_id = ' . $id;
+        return $this->find_row($sql);
+    }
+
+    public function find_user($id)
+    {
+		$sql = 'SELECT u.* FROM ' . USERS_TABLE . ' u WHERE user_id=' . $id;
+        return $this->find_row($sql);
+    }
+
+    public function find_row($sql)
+    {
+        $sql .= ' LIMIT 1';
+        $row = $this->get_rows($sql);
+        return @$row[0];
+    }
+
+    public function get_topic($id)
+    {
+        $sql = 'SELECT t.*, f.*, u.* FROM ';
+        $sql .= TOPICS_TABLE . ' t ';
+        $sql .= 'LEFT JOIN ' . USERS_TABLE . ' u ON t.topic_poster = u.user_id ';
+        $sql .= 'LEFT JOIN ' . FORUMS_TABLE . ' f ON t.forum_id = f.forum_id ';
+        $sql .= 'WHERE t.topic_id = ' . (int)$id;
+
+        return $this->find_row($sql);
+    }
+
+    public function get_rows($sql)
+    {
+        global $db;
+		$result = $db->sql_query($sql);
+        $rows = array();
+		while ($row = $db->sql_fetchrow($result))
+        {
+            $rows[] = $row;
+        }
+        $db->sql_freeresult($result);
+        return $rows;
+    }
+
+    public function execute($sql)
+    {
+        global $db;
+        $db->sql_query($sql);
+    }
+}
+// }}}
+
+// Mappers {{{
+
+// Message -> Post (has to be have been joined with forums (to get the forum as category))
+// !!! will not have user filled in
+function post_to_sdk($data) {
+    return array(
+        'body' => $data['post_text'],
+        'categories' => array($data['forum_name']),
+        'created_at' => ftime($data['post_time']),
+        'custom_actions' => array(
+            'messages_warn' => array('name' => 'Warn', 'params' => array())
+        ),
+        'custom_fields' => array(
+            'post_checksum' => $data['post_checksum']
+        ),
+        'display_url' => '',
+        'latitude' => '',
+        'longitude' => '',
+        'id' => $data['post_id'],
+        'in_reply_to_id' => null,
+        'ip' => $data['poster_ip'],
+        'title' => $data['post_subject'],
+        'thread_id' => $data['topic_id'],
+        'updated_at' => empty($data['post_edit_time']) ? ftime($data['post_time']) : ftime($data['post_edit_time'])
+    );
+}
+
+// Has to be joined with the first post of the thread (to have a body), and with forums (to get the forum_name for categories)
+// !!! will not have user filled in
+function topic_to_sdk($data) {
+    return array(
+        'categories' => array($data['forum_name']),
+        'created_at' => ftime($data['topic_time']),
+        'custom_actions' => array(),
+        'custom_fields' => array(),
+        'display_url' => '',
+        'latitude' => null,
+        'longitude' => null,
+        'id' => $data['topic_id'],
+        'ip' => null,
+        'title' => $data['topic_title'],
+        'updated_at' => ftime($data['topic_last_post_time'])
+    );
+}
+
+// !!! will not have "author" and "recipient" filled in
+function privmsg_to_sdk($data) {
+    return array(
+        'body' => $data['message_text'],
+        'created_at' => ftime($data['message_time']),
+        'custom_actions' => array(),
+        'custom_fields' => array(),
+        'latitude' => null,
+        'longitude' => null,
+        'id' => $data['msg_id'],
+        'in_reply_to_id' => $data['root_level'],
+        'ip' => $data['author_ip'],
+        'title' => $data['message_subject'],
+        'updated_at' => empty($data['message_edit_time']) ? ftime($data['message_time']) : ftime($data['message_edit_time'])
+    );
+}
+
+function user_to_sdk($data) {
+    return array(
+        'avatar_url' => $data['user_avatar'],
+        'created_at' => ftime($data['user_regdate']),
+        'custom_fields' => array('signature' => $data['user_sig']),
+        'email' => $data['user_email'],
+        'latitude' => null,
+        'longitude' => null,
+        'firstname' => null,
+        'home_phone' => null,
+        'id' => $data['user_id'],
+        'ip' => $data['user_ip'],
+        'lastname' => null,
+        'mobile_phone' => null,
+        'notes' => null,
+        'puppetizable' => true,
+        'screenname' => $data['username'],
+        'updated_at' => null
+    );
+}
+
+function ftime($timestamp)
+{
+    return strftime('%Y-%m-%d %H:%M:%S', $timestamp);
+}
+
+// }}}
+
+// Stubs {{{
+/**
+ * This is just a stub so that I can use functions like submit_post
+ * without having to authenticate the user.
+ */
+class smcc_sdk_auth
+{
+    public function acl_get()
+    {
+        return true;
+    }
+
+    public function acl()
+    {
+    }
+}
+// }}}
+
---
+++ samples/wordpress.php
@@ -0,0 +1,371 @@
+<?php
+/**
+ * @package SMCC_SDK
+ * @version 1.0
+ */
+/*
+Plugin Name: SMCC SDK
+Plugin URI: http://wordpress.org/extend/plugins/smcc-sdk/
+Description: Example implementation of the Dimelo SDK.
+Author: Dimelo
+Version: 1.0
+Author URI: http://www.dimelo.com/
+License: GPL2
+*/
+
+define('SMCC_SDK_ACCESS_TOKEN', 'EilW295myMpycBTQkaxeyEMhccBGs9yII4MgV4kwb6CTgcCUpOdOsUj5m1wiWZ7');
+
+// Handle the request if it is SDK related, do nothing otherwise.
+add_action( 'init', array(new SmccSdkApi, 'respond') );
+
+class SmccSdkApi { // {{{
+
+    protected $implemented_actions = array('messages.create', 'messages.list', 'messages.show', 'messages.destroy', 'threads.create', 'threads.destroy', 'threads.show');
+    protected $implemented_options = array('messages.no_title');
+
+    public function __construct() {
+        $this->db = new SmccSdkDb();
+        $this->format = new SmccSdkFormat();
+    }
+
+    public function respond() {
+        if (!$this->is_sdk_request()) {
+            return;
+        }
+
+        $this->validate_token();
+
+        $action = $this->get_action();
+        if ($action == 'implementation.info' || in_array($action, $this->implemented_actions)) {
+            $method = str_replace('.', '_', $action);
+            $this->$method();
+        } else {
+            $this->error_response('Invalid action');
+        }
+    }
+
+    public function get_action() {
+        return @$_GET['smcc_sdk_action'];
+    }
+
+    public function get_body() {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    public function get_id() {
+        return @$_GET['smcc_sdk_id'];
+    }
+
+    public function get_since_id() {
+        return @$_GET['smcc_sdk_since_id'];
+    }
+
+    public function is_sdk_request() {
+        $action = $this->get_action();
+        return !empty($action);
+    }
+
+    public function validate_token() {
+        $token = @$_GET['smcc_sdk_access_token'];
+        if ($token != SMCC_SDK_ACCESS_TOKEN) {
+            $this->error_response('Invalid access token');
+        }
+    }
+
+    public function success_response($payload) {
+        $this->output(array('success' => true, 'payload' => $payload));
+    }
+
+    public function error_response($message) {
+        $this->output(array('success' => false, 'errorMessage' => $message));
+    }
+
+    public function output($array) {
+        header('Content-type: application/json');
+        echo json_encode($array);
+        exit;
+    }
+
+    public function implementation_info() {
+        $this->success_response(array(
+            'actions' => $this->implemented_actions,
+            'options' => $this->implemented_options
+        ));
+    }
+
+    public function messages_create() {
+        $message = $this->get_body();
+
+        $data = array(
+            'comment_post_ID' => $message['thread_id'],
+            'comment_content' => $message['body'],
+            'comment_parent' => $message['in_reply_to_id'],
+            'comment_date' => $message['created_at'],
+            'comment_approved' => 1
+        );
+
+        $user = $this->db->get_user($message['author_id']);
+        if (empty($user)) {
+            $user = $this->db->get_comment_user($message['author_id']);
+            if (empty($user)) {
+                $this->error_response('Could not create the comment');
+            }
+            $data['comment_author'] = $user['comment_author'];
+            $data['comment_author_email'] = $user['comment_author_email'];
+            $data['comment_author_url'] = $user['comment_author_url'];
+
+            $formatted_user = $this->format->comment_user($user);
+        } else {
+            $data['user_id'] = $user['ID'];
+            $data['comment_author'] = $user['user_nicename'];
+            $data['comment_author_email'] = $user['user_email'];
+            $data['comment_author_url'] = $user['user_url'];
+
+            $formatted_user = $this->format->user($user);
+        }
+
+        $comment = $this->db->comments_create($data);
+        if (!$comment) {
+            $this->error_response('Could not create the comment');
+        } else {
+            $this->success_response($this->format->message($comment, $formatted_user));
+        }
+    }
+
+    public function messages_destroy() {
+        $response = $this->db->delete_comment($this->get_id());
+        $this->success_response($response ? true : false);
+    }
+
+    public function messages_list() {
+        $messages = array();
+        $last_id = $this->get_since_id();
+        foreach ($this->db->get_comments_since($last_id) as $comment) {
+            if (!empty($comment['user_id'])) {
+                $user = $this->format->user($comment);
+            } else {
+                $user = $this->format->comment_user($comment);
+            }
+            if ($user) {
+                $messages[] = $this->format->message($comment, $user);
+            }
+        }
+        $this->success_response($messages);
+    }
+
+    public function messages_show() {
+        $comment = $this->db->get_comment($this->get_id());
+        if (!empty($comment['user_id'])) {
+            $user = $this->format->user($comment);
+        } else {
+            $user = $this->format->comment_user($comment);
+        }
+        if ($user) {
+            $this->success_response($this->format->message($comment, $user));
+        } else {
+            $this->error_response('Invalid user');
+        }
+    }
+
+    public function threads_create() {
+        $post = $this->db->posts_create($this->get_body());
+        $user = $this->db->get_user($post['post_author']);
+
+        if (!$post) {
+            $this->error_response('Could not create post');
+        } else {
+            $this->success_response($this->format->thread($post, $this->format->user($user)));
+        }
+    }
+
+    public function threads_destroy() {
+        $response = $this->db->posts_delete($this->get_id());
+        $this->success_response($response ? true : false);
+    }
+
+    public function threads_show() {
+        $post = $this->db->get_post($this->get_id());
+        $user = $this->db->get_user($post['post_author']);
+
+        $this->success_response($this->format->thread($post, $this->format->user($user)));
+    }
+
+}
+// }}}
+
+/**
+ * Converts from Wordpress data to SMCC SDK data structures.
+ */
+class SmccSdkFormat { // {{{
+
+    public function thread($post, $user) {
+        return array(
+            'author' => $user,
+            'body' => $post['post_content'],
+            'categories' => array(),
+            'created_at' => $post['post_date'],
+            'custom_actions' => array(),
+            'custom_fields' => array(),
+            'title' => $post['post_title'],
+            'id' => $post['ID'],
+            'updated_at' => $post['post_modified']
+        );
+    }
+
+    public function message($comment, $user) {
+        $attributes = array(
+            'author' => $user,
+            'body' => $comment['comment_content'],
+            'categories' => array(),
+            'created_at' => $comment['comment_date'],
+            'custom_actions' => array(),
+            'custom_fields' => array(),
+            'latitude' => null,
+            'longitude' => null,
+            'id' => $comment['comment_ID'],
+            'in_reply_to_id' => $comment['comment_parent'],
+            'ip' => $comment['comment_author_IP'],
+            'thread_id' => $comment['comment_post_ID'],
+            'updated_at' => null
+        );
+        return $attributes;
+    }
+
+    public function user($user) {
+        return array(
+            'avatar_url' => null,
+            'created_at' => $user['user_registered'],
+            'email' => $user['user_email'],
+            'latitude' => null,
+            'longitude' => null,
+            'firstname' => null,
+            'home_phone' => null,
+            'id' => $user['ID'],
+            'ip' => null,
+            'lastname' => null,
+            'mobile_phone' => null,
+            'notes' => null,
+            'puppetizable' => true,
+            'screenname' => $user['display_name'],
+            'updated_at' => null,
+            'url' => $user['user_url']
+        );
+    }
+
+    public function comment_user($comment) {
+        if (empty($comment['comment_author_email'])) {
+            return null;
+        }
+
+        return array(
+            'created_at' => $comment['comment_date'],
+            'email' => $comment['comment_author_email'],
+            'id' => $comment['comment_author_email'],
+            'ip' => $comment['comment_author_IP'],
+            'puppetizable' => false,
+            'screenname' => $comment['comment_author'],
+            'url' => $comment['comment_author_url']
+        );
+    }
+
+}
+// }}}
+
+class SmccSdkDb { // {{{
+
+    public function __construct() {
+    }
+
+    public function get_comments_since($last_comment_id = null) {
+        $last_comment_id = empty($last_comment_id) ? 0 : $last_comment_id;
+        $sql = "SELECT * FROM {$this->table_comments()} as t LEFT JOIN {$this->table_users()} as u ON t.user_id = u.ID WHERE t.comment_id > %d ORDER BY t.comment_id DESC";
+        return $this->find($sql, $last_comment_id);
+    }
+
+    public function get_comment($id) {
+        $sql = "SELECT * FROM {$this->table_comments()} WHERE comment_id = %d";
+        return $this->find_first($sql, $id);
+    }
+
+    public function delete_comment($id) {
+        $sql = "DELETE FROM {$this->table_comments()} WHERE comment_id = %d";
+        return $this->query($sql, $id);
+    }
+
+    public function comments_create($data) {
+        $id = wp_insert_comment($data);
+        return $this->get_comment($id);
+    }
+
+    public function get_user($id) {
+        $sql = "SELECT * FROM {$this->table_users()} WHERE ID = %d";
+        return $this->find_first($sql, $id);
+    }
+
+    public function get_comment_user($email) {
+        $sql = "SELECT * FROM {$this->table_comments()} WHERE comment_author_email = %s ORDER BY comment_ID DESC";
+        return $this->find_first($sql, $email);
+    }
+
+    public function get_post($id) {
+        $sql = "SELECT * FROM {$this->table_posts()} WHERE id = %d";
+        return $this->find_first($sql, $id);
+    }
+
+    public function posts_create($message) {
+        $data = array(
+            'post_author' => $message['author_id'],
+            'post_content' => $message['body'],
+            'post_date' => $message['created_at'],
+            'post_status' => 'publish',
+            'post_title' => $message['title'],
+            'post_type' => 'post'
+        );
+
+        $id = wp_insert_post($data);
+        return $this->get_post($id);
+    }
+
+    public function posts_delete($id) {
+        return wp_delete_post($id, true);
+    }
+
+    // Helpers {{{
+
+    protected function find($query, $values = array()) {
+        global $wpdb;
+        return $wpdb->get_results($wpdb->prepare($query, $values), ARRAY_A);
+    }
+
+    protected function find_first() {
+        $args = func_get_args();
+        $result = call_user_func_array(array($this, 'find'), $args);
+        return empty($result) ? null : $result[0];
+    }
+
+    protected function query($query, $values = array()) {
+        global $wpdb;
+        $result = $wpdb->query($wpdb->prepare($query, $values));
+        return $result !== false && $result > 0;
+    }
+
+    protected function table_comments() {
+        global $wpdb;
+        return $wpdb->comments;
+    }
+
+    protected function table_posts() {
+        global $wpdb;
+        return $wpdb->posts;
+    }
+
+    protected function table_users() {
+        global $wpdb;
+        return $wpdb->users;
+    }
+
+    // }}}
+
+}
+// }}}
+
