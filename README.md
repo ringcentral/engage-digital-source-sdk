d303e031cb2931c3afaa48d4dc5ec48145b8205e
Adrian Gheorghe <adrian.gheorghe@gmail.com>
2012-10-09 09:53:09 +0200

    Updated readme to include some useful pointers; Removed tabs from the php sdk

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Please see the [[wiki]] for documentation.
+# SMCC SDK
+
+* Please see the [Wiki](https://github.com/dimelo/sdk/wiki) for documentation.
+* Look inside the samples folder for example implementations for open source projects.
+* Study or use the PHP SDK implementation inside the php folder.
--- php/sdk.php
+++ php/sdk.php
@@ -22,26 +22,26 @@ if (!function_exists('json_encode')) {
 
 class SmccSdk {
 
-	/**
-	 * Key used for signing communication.
-	 */
+    /**
+     * Key used for signing communication.
+     */
     protected $secret_key = null;
 
-	/**
-	 * Request body in original form.
-	 */
+    /**
+     * Request body in original form.
+     */
     protected $request_body = null;
 
-	/**
-	 * JSON decoded body of the request.
-	 */
+    /**
+     * JSON decoded body of the request.
+     */
     protected $decoded_request_body = null;
 
-	/**
-	 * Create an SDK object with the secret key.
-	 *
-	 * @param string $key Secret key used for communication signing.
-	 */
+    /**
+     * Create an SDK object with the secret key.
+     *
+     * @param string $key Secret key used for communication signing.
+     */
     public function __construct($key) {
         $this->secret_key = $key;
     }
@@ -53,64 +53,64 @@ class SmccSdk {
      * Please make sure to call `date_default_timezone_set()` before calling
      * time functions.
      *
-	 * @param string $timestr Time string in any format supported by `strtotime`.
-	 *
+     * @param string $timestr Time string in any format supported by `strtotime`.
+     *
      * @return string Date in the ISO8601 format.
      */
     public static function to_sdk_time($timestr) {
         return date(DATE_ISO8601, strtotime($timestr));
     }
 
-	/**
-	 * Takes a SDK time string and returns a string in the expected `$format`.
-	 * The default format is `Y-m-d H:i:s`.
-	 *
+    /**
+     * Takes a SDK time string and returns a string in the expected `$format`.
+     * The default format is `Y-m-d H:i:s`.
+     *
      * Please make sure to call `date_default_timezone_set()` before calling
      * time functions.
      *
-	 * @param string $timestr Time string in the ISO8601 format.
-	 * @param string $format  Format to convert the date to. Default is `Y-m-d H:i:s`.
-	 *
-	 * @return string Date in the specified format.
-	 */
+     * @param string $timestr Time string in the ISO8601 format.
+     * @param string $format  Format to convert the date to. Default is `Y-m-d H:i:s`.
+     *
+     * @return string Date in the specified format.
+     */
     public static function from_sdk_time($timestr, $format = 'Y-m-d H:i:s') {
         return date($format, strtotime($timestr));
     }
 
-	/**
-	 * Returns the action specified by the current request.
-	 *
-	 * @return string SDK action.
-	 */
+    /**
+     * Returns the action specified by the current request.
+     *
+     * @return string SDK action.
+     */
     public function get_action() {
         $body = $this->get_body();
         return $body['action'];
     }
 
-	/**
-	 * Returns the parameters passed for the SDK action.
-	 *
-	 * @return array SDK action's parameters.
-	 */
+    /**
+     * Returns the parameters passed for the SDK action.
+     *
+     * @return array SDK action's parameters.
+     */
     public function get_params() {
         $body = $this->get_body();
         return array_key_exists('params', $body) ? $body['params'] : array();
     }
 
-	/**
-	 * Checks if the signature matches the request body.
-	 *
-	 * @return bool
-	 */
+    /**
+     * Checks if the signature matches the request body.
+     *
+     * @return bool
+     */
     public function is_valid_request() {
         return $this->signature($this->get_raw_body()) === @$_SERVER['HTTP_X_SMCCSDK_SIGNATURE'];
     }
 
-	/**
-	 * JSON encodes the `$object` and outputs it. It also computes the
-	 * signature for the response and sends it as a header.
-	 * Note: this function will call `exit`.
-	 */
+    /**
+     * JSON encodes the `$object` and outputs it. It also computes the
+     * signature for the response and sends it as a header.
+     * Note: this function will call `exit`.
+     */
     public function respond($object) {
         $body = json_encode($object);
         header('Content-type: application/json');
@@ -119,35 +119,35 @@ class SmccSdk {
         exit;
     }
 
-	/**
-	 * Sends a 400 header and outputs the given `$message`.
-	 * Note: this function will call `exit`.
-	 *
-	 * @param string $message Error message.
-	 */
+    /**
+     * Sends a 400 header and outputs the given `$message`.
+     * Note: this function will call `exit`.
+     *
+     * @param string $message Error message.
+     */
     public function error($message) {
         header('Status: 400 Bad Request');
         echo $message;
         exit;
     }
 
-	/**
-	 * Computes the SHA512 signature of the given `$text` using the
-	 * secret key.
-	 *
-	 * @param string $text String to be signed.
-	 *
-	 * @return string Signature string.
-	 */
+    /**
+     * Computes the SHA512 signature of the given `$text` using the
+     * secret key.
+     *
+     * @param string $text String to be signed.
+     *
+     * @return string Signature string.
+     */
     public function signature($text) {
         return hash_hmac('sha512', $text, $this->secret_key, $raw = false);
     }
 
-	/**
-	 * Returns the body of a POST request.
-	 *
-	 * @return string POST body.
-	 */
+    /**
+     * Returns the body of a POST request.
+     *
+     * @return string POST body.
+     */
     protected function get_raw_body() {
         if ($this->request_body === null) {
             $this->request_body = file_get_contents('php://input');
@@ -155,11 +155,11 @@ class SmccSdk {
         return $this->request_body;
     }
 
-	/**
-	 * Returns the JSON decoded representation of the request body.
-	 *
-	 * @return object JSON decode POST body.
-	 */
+    /**
+     * Returns the JSON decoded representation of the request body.
+     *
+     * @return object JSON decode POST body.
+     */
     protected function get_body() {
         if ($this->decoded_request_body === null) {
             $this->decoded_request_body = json_decode($this->get_raw_body(), true);
